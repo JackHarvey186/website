3a05ef09ace85af04b3ee49352fc03079c17f0c3
Jack <114150402+JackHarvey186@users.noreply.github.com>
2023-03-28 10:34:44 +0100

    404 Page with rotating text

---
+++ 404.js
@@ -0,0 +1,24 @@
+function e404() {
+    var e404s = [
+        "You've taken a wrong turn!",
+        "Make a U-Turn when Possible.",
+        "Error Message Here",
+        "Wait, did I screw up or did you? I vote you.",
+        "Welcome to the Backrooms of JHarvey.uk... Snowcone?",
+        "Uh Uh Uh! You didn't say the magic word!",
+        "We've blown a fuse!",
+        "We're sorry, but the site you requested is no longer of this reality. Please try again.",
+        "This page has been taken down by request of the Time Variance Authority.",
+        "We rolled a nat 1!",
+        "Landed in the snow, but there's no vole!",
+        "Apologies, but due to intergalactic sanctions, this page is unavaiable in any language except Otusian.",
+    ];
+    var randomIndex = Math.floor(Math.random() * e404s.length); 
+    var randomElement = e404s[randomIndex];
+    return randomElement;
+    }
+
+    var e404Element = document.getElementById("404s");
+    if (e404Element) {
+      motdElement.innerHTML = e404();
+    }
